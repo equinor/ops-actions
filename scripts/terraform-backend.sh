6df56dc25a07b72e2479cabcb5ced853a4876a0d
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-09-09 09:46:09 +0200

    feat(terraform): add backend script (#32)
    Add script for creating a remote AzureRM backend for Terraform.

---
+++ scripts/terraform-backend.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Creates an Azure storage account for storing remote Terraform state.
+# Requires Azure role 'Owner' at the subscription scope.
+# Arguments:
+#   Azure region to create the storage account in, a name.
+#   Azure AD service principal that should manage the remote Terraform state, a client ID.
+# Outputs:
+#   Terraform backend configuration.
+
+set -eu
+
+readonly RESOURCE_GROUP_NAME='tfstate'
+readonly LOCATION="$1"
+readonly STORAGE_ACCOUNT_NAME="tfstate${RANDOM}"
+readonly CONTAINER_NAME='tfstate'
+readonly SP_CLIENT_ID="$2"
+
+az group create \
+  --name "${RESOURCE_GROUP_NAME}" \
+  --location "${LOCATION}" \
+  --output none
+
+storage_account_id="$(az storage account create \
+  --name "${STORAGE_ACCOUNT_NAME}" \
+  --resource-group "${RESOURCE_GROUP_NAME}" \
+  --location "${LOCATION}" \
+  --sku Standard_GRS \
+  --access-tier Hot \
+  --kind BlobStorage \
+  --https-only true \
+  --min-tls-version TLS1_2 \
+  --allow-blob-public-access false \
+  --allow-shared-key-access false \
+  --query id \
+  --output tsv)"
+
+az storage account blob-service-properties update \
+  --account-name "${STORAGE_ACCOUNT_NAME}" \
+  --resource-group "${RESOURCE_GROUP_NAME}" \
+  --enable-delete-retention true \
+  --delete-retention-days 30 \
+  --enable-container-delete-retention true \
+  --container-delete-retention-days 30 \
+  --enable-versioning true \
+  --enable-change-feed true \
+  --output none
+
+az security atp storage update \
+  --storage-account "${STORAGE_ACCOUNT_NAME}" \
+  --resource-group "${RESOURCE_GROUP_NAME}" \
+  --is-enabled true \
+  --output none
+
+az storage container create \
+  --name "${CONTAINER_NAME}" \
+  --account-name "${STORAGE_ACCOUNT_NAME}" \
+  --auth-mode login \
+  --output none
+
+sp_id="$(az ad sp show \
+  --id "${SP_CLIENT_ID}" \
+  --query id \
+  --output tsv)"
+
+az role assignment create \
+  --assignee "${sp_id}" \
+  --role 'Storage Blob Data Owner' \
+  --scope "${storage_account_id}" \
+  --output none
+
+az resource lock create \
+  --name 'Terraform' \
+  --lock-type ReadOnly \
+  --resource "${storage_account_id}" \
+  --notes 'Prevent changes to Terraform backend configuration' \
+  --output none
+
+echo "resource_group_name = \"${RESOURCE_GROUP_NAME}\"
+storage_account_name = \"${STORAGE_ACCOUNT_NAME}\"
+container_name = \"${CONTAINER_NAME}\"
+use_azuread_auth = true"
