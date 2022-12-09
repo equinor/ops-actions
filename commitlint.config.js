0246188b530b9fb6f7034872a65a28940d3aa5bc
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-12-09 13:36:37 +0100

    chore: test valid scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 const Configuration = {
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'wip', 'build', 'ci', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']],
-    'scope-enum': [2, 'always', []]
+    'scope-enum': [2, 'always', ['test']]
   }
 }
 
