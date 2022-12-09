6bfa1f10b71eb8c61d4f1dba5a32cca96a2ba3a6
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-12-09 13:33:12 +0100

    fix: remove unused config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,6 @@
 const Configuration = {
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'wip', 'build', 'ci', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']],
-    'scope-enum': []
+    'type-enum': [2, 'always', ['feat', 'fix', 'wip', 'build', 'ci', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']]
   }
 }
 
