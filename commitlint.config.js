4555b1bf7ea5ead1df8b6c2b6b0d413193e88750
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-12-09 12:54:47 +0100

    feat(commitlint): set configuration

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+const Configuration = {
+  rules: {
+    'type-enum': [
+      'feat',
+      'fix',
+      'wip',
+      'build',
+      'ci',
+      'docs',
+      'style',
+      'refactor',
+      'perf',
+      'test',
+      'chore'
+    ],
+    'scope-enum': []
+  }
+}
+
+module.exports = Configuration;
