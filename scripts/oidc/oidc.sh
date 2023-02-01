908133feffb48456312143c86892981b46039df5
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-02-01 08:54:18 +0100

    chore: consistent syntax (#102)

--- scripts/oidc/oidc.sh
+++ scripts/oidc/oidc.sh
@@ -7,9 +7,9 @@ export REPO="$2"
 export ENVIRONMENT="$3"
 CONFIG_FILE="$4"
 
-account=$(az account show --query '{subscriptionName:name, subscriptionId:id, tenantId:tenantId}' --output json)
+account=$(az account show --query "{subscriptionName:name, subscriptionId:id, tenantId:tenantId}" --output json)
 
-subscription_name=$(jq -r '.subscriptionName' <<< "$account")
+subscription_name=$(jq -r .subscriptionName <<< "$account")
 read -r -p "Configure OIDC from GitHub repo '$REPO' to Azure subscription '$subscription_name'? (y/N) " response
 case $response in
   [yY][eE][sS]|[yY])
@@ -19,68 +19,67 @@ case $response in
     ;;
 esac
 
-SUBSCRIPTION_ID=$(jq -r '.subscriptionId' <<< "$account")
+SUBSCRIPTION_ID=$(jq -r .subscriptionId <<< "$account")
 export SUBSCRIPTION_ID
 echo "SUBSCRIPTION_ID: $SUBSCRIPTION_ID"
 
-tenant_id=$(jq -r '.tenantId' <<< "$account")
+tenant_id=$(jq -r .tenantId <<< "$account")
 echo "TENANT_ID: $tenant_id"
 
-echo 'Checking if application already exists...'
+echo "Checking if application already exists..."
 app_id=$(az ad app list --filter "displayName eq '$APP_NAME'" --query [].appId --output tsv)
-if [[ -z "$app_id" ]]
-then
-  echo 'Creating application...'
+if [[ -z "$app_id" ]]; then
+  echo "Creating application..."
   app_id=$(az ad app create --display-name "$APP_NAME" --query appId --output tsv)
 else
-  echo 'Using existing application.'
+  echo "Using existing application."
 fi
 echo "CLIENT_ID: $app_id"
 
-echo 'Reading config...'
+echo "Reading config..."
 config=$(envsubst < "$CONFIG_FILE")
 
-echo 'Checking if federated identity credential already exists...'
-fic=$(echo "$config" | jq '.federatedCredential + {"issuer": "https://token.actions.githubusercontent.com", "audiences": ["api://AzureADTokenExchange"]}')
-fic_name=$(echo "$fic" | jq -r '.name')
+echo "Checking if federated identity credential already exists..."
+fic=$(jq '.federatedCredential + {"issuer": "https://token.actions.githubusercontent.com", "audiences": ["api://AzureADTokenExchange"]}' <<< "$config")
+fic_name=$(jq -r .name <<< "$fic")
 fic_id=$(az ad app federated-credential list --id "$app_id" --query "[?name == '$fic_name'].id" --output tsv)
 if [[ -z "$fic_id" ]]
 then
-  echo 'Creating federated identity credential...'
+  echo "Creating federated identity credential..."
   az ad app federated-credential create --id "$app_id" --parameters "$fic" --output none
 else
-  echo 'Updating existing federated identity credential.'
+  echo "Updating existing federated identity credential."
   az ad app federated-credential update --id "$app_id" --federated-credential-id "$fic_id" --parameters "$fic" --output none
 fi
 
-echo 'Checking if service principal already exists...'
+echo "Checking if service principal already exists..."
 sp_id=$(az ad sp list --filter "appId eq '$app_id'" --query [].id --output tsv)
 if [[ -z "$sp_id" ]]
 then
-  echo 'Creating service principal...'
+  echo "Creating service principal..."
   sp_id=$(az ad sp create --id "$app_id" --query id --output tsv)
 else
-  echo 'Using existing service principal.'
+  echo "Using existing service principal."
 fi
 echo "OBJECT_ID: $sp_id"
 
-echo 'Creating role assignments...'
-ras=$(echo "$config" | jq -c '.roleAssignments[]')
-echo "$ras" | while read -r ra; do
-  role=$(echo "$ra" | jq -r '.role')
-  scope=$(echo "$ra" | jq -r '.scope')
+echo "Creating role assignments..."
+ras=$(jq -c .roleAssignments[] <<< "$config")
+while read -r ra; do
+  role=$(jq -r .role <<< "$ra")
+  scope=$(jq -r .scope <<< "$ra")
   echo "Assigning role '$role' at scope '$scope'..."
-  az role assignment create --role "$role" --subscription "$SUBSCRIPTION_ID" --assignee-object-id "$sp_id" --assignee-principal-type ServicePrincipal --scope "$scope" --output none
-done
+  az role assignment create --role "$role" --assignee-object-id "$sp_id" --assignee-principal-type ServicePrincipal --scope "$scope" --output none
+done <<< "$ras"
 
 if [[ -n "$ENVIRONMENT" ]]; then
-  echo 'Creating GitHub environment...'
+  echo "Creating GitHub environment..."
   gh api --method PUT "repos/$REPO/environments/$ENVIRONMENT"
   # GitHub CLI does not natively support creating environments (cli/cli#5149).
   # Create using GitHub API request instead.
 fi
 
-echo 'Updating GitHub secrets...'
-gh secret set 'AZURE_CLIENT_ID' --body "$app_id" --repo "$REPO" --env "$ENVIRONMENT"
-gh secret set 'AZURE_SUBSCRIPTION_ID' --body "$SUBSCRIPTION_ID" --repo "$REPO" --env "$ENVIRONMENT"
-gh secret set 'AZURE_TENANT_ID' --body "$tenant_id" --repo "$REPO" --env "$ENVIRONMENT"
+echo "Updating GitHub secrets..."
+gh secret set "AZURE_CLIENT_ID" --body "$app_id" --repo "$REPO" --env "$ENVIRONMENT"
+gh secret set "AZURE_SUBSCRIPTION_ID" --body "$SUBSCRIPTION_ID" --repo "$REPO" --env "$ENVIRONMENT"
+gh secret set "AZURE_TENANT_ID" --body "$tenant_id" --repo "$REPO" --env "$ENVIRONMENT"
