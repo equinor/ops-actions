30f134d6f57a93ca911ff7e66f70c37f32ca7f92
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-05-05 11:15:57 +0200

    chore: add line continuation

--- scripts/oidc/oidc.sh
+++ scripts/oidc/oidc.sh
@@ -44,15 +44,20 @@ config=$(envsubst < "$CONFIG_FILE")
 # Create Azure AD application
 ################################################################################
 
-app_id=$(az ad app list --filter "displayName eq '$APP_NAME'" \
-  --query "[].appId" --output tsv)
+app_id=$(az ad app list \
+  --filter "displayName eq '$APP_NAME'" \
+  --query "[].appId" \
+  --output tsv)
 
 if [[ -z "$app_id" ]]
 then
   echo "Creating application..."
 
-  app_id=$(az ad app create --display-name "$APP_NAME" \
-    --sign-in-audience AzureADMyOrg --query appId --output tsv)
+  app_id=$(az ad app create \
+    --display-name "$APP_NAME" \
+    --sign-in-audience AzureADMyOrg \
+    --query appId \
+    --output tsv)
 else
   echo "Using existing application."
 fi
@@ -70,8 +75,10 @@ while read -r fic
 do
   fic_name=$(jq -r .name <<< "$fic")
 
-  fic_id=$(az ad app federated-credential list --id "$app_id" \
-    --query "[?name == '$fic_name'].id" --output tsv)
+  fic_id=$(az ad app federated-credential list \
+    --id "$app_id" \
+    --query "[?name == '$fic_name'].id" \
+    --output tsv)
 
   parameters=$(jq '{
     "name": .name,
@@ -85,13 +92,17 @@ do
   then
     echo "Creating federated identity credential '$fic_name'..."
 
-    az ad app federated-credential create --id "$app_id" \
-      --parameters "$parameters" --output none
+    az ad app federated-credential create \
+      --id "$app_id" \
+      --parameters "$parameters" \
+      --output none
   else
     echo "Updating existing federated identity credential '$fic_name'..."
 
-    az ad app federated-credential update --id "$app_id" \
-      --federated-credential-id "$fic_id" --parameters "$parameters" \
+    az ad app federated-credential update \
+      --id "$app_id" \
+      --federated-credential-id "$fic_id" \
+      --parameters "$parameters" \
       --output none
   fi
 
@@ -111,13 +122,19 @@ done <<< "$fics"
 # Create Azure AD service principal
 ################################################################################
 
-sp_id=$(az ad sp list --filter "appId eq '$app_id'" --query [].id --output tsv)
+sp_id=$(az ad sp list \
+  --filter "appId eq '$app_id'" \
+  --query [].id \
+  --output tsv)
 
 if [[ -z "$sp_id" ]]
 then
   echo "Creating service principal..."
 
-  sp_id=$(az ad sp create --id "$app_id" --query id --output tsv)
+  sp_id=$(az ad sp create \
+    --id "$app_id" \
+    --query id \
+    --output tsv)
 else
   echo "Using existing service principal."
 fi
@@ -135,8 +152,12 @@ do
 
   echo "Assigning role '$role' at scope '$scope'..."
 
-  az role assignment create --role "$role" --assignee-object-id "$sp_id" \
-    --assignee-principal-type ServicePrincipal --scope "$scope" --output none
+  az role assignment create \
+    --role "$role" \
+    --assignee-object-id "$sp_id" \
+    --assignee-principal-type ServicePrincipal \
+    --scope "$scope" \
+    --output none
 done <<< "$ras"
 
 ################################################################################
@@ -149,11 +170,17 @@ if [[ "$repo_level" == true ]]
 then
   echo "Setting GitHub repository secrets..."
 
-  gh secret set "AZURE_CLIENT_ID" --repo "$REPO" --body "$app_id"
+  gh secret set "AZURE_CLIENT_ID" \
+    --repo "$REPO" \
+    --body "$app_id"
 
-  gh secret set "AZURE_SUBSCRIPTION_ID" --repo "$REPO" --body "$SUBSCRIPTION_ID"
+  gh secret set "AZURE_SUBSCRIPTION_ID" \
+    --repo "$REPO" \
+    --body "$SUBSCRIPTION_ID"
 
-  gh secret set "AZURE_TENANT_ID" --repo "$REPO" --body "$tenant_id"
+  gh secret set "AZURE_TENANT_ID" \
+    --repo "$REPO" \
+    --body "$tenant_id"
 fi
 
 ################################################################################
@@ -165,16 +192,24 @@ do
   echo "Creating GitHub environment '$env'..."
 
   # GitHub CLI does not natively support creating environments (cli/cli#5149).
-  # Create using GitHub API request instead.
-  gh api "repos/$REPO/environments/$env" --method PUT --silent
+  gh api "repos/$REPO/environments/$env" \
+    --method PUT \
+    --silent
 
   echo "Setting GitHub environment secrets for environment '$env'..."
 
-  gh secret set "AZURE_CLIENT_ID" --repo "$REPO" --env "$env" --body "$app_id"
+  gh secret set "AZURE_CLIENT_ID" \
+    --repo "$REPO" \
+    --env "$env" \
+    --body "$app_id"
 
-  gh secret set "AZURE_SUBSCRIPTION_ID" --repo "$REPO" --env "$env" \
+  gh secret set "AZURE_SUBSCRIPTION_ID" \
+    --repo "$REPO" \
+    --env "$env" \
     --body "$SUBSCRIPTION_ID"
 
-  gh secret set "AZURE_TENANT_ID" --repo "$REPO" --env "$env" \
+  gh secret set "AZURE_TENANT_ID" \
+    --repo "$REPO" \
+    --env "$env" \
     --body "$tenant_id"
 done
