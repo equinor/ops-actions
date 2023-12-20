c87c0221aed222a0ccf04636641407b359894f0c
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-20 14:01:03 +0100

    wip

--- scripts/terraform-ci/test.py
+++ scripts/terraform-ci/test.py
@@ -4,7 +4,9 @@
 import argparse
 import os
 import glob
+import json
 
+import yaml
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", type=str, default=".")
@@ -12,15 +14,48 @@ args = parser.parse_args()
 path = args.path
 
 terraformPath = os.path.join(path, "terraform")
-dirs = []
+modules = os.listdir(terraformPath)
 
-files = glob.glob(terraformPath + "/**/*.tf", recursive=True)
-modulesPath = os.path.join(terraformPath, "modules")
-for file in files:
-    # Ignore module files
-    if not file.startswith(modulesPath):
-        dir = os.path.dirname(file)
-        if dir not in dirs:
-            dirs.append(dir)
+# This script only cares about root modules.
+# Exclude directory containing reusable modules.
+if "modules" in modules:
+    modules.remove("modules")
 
-print(dirs)
+for mod in modules:
+    servicePath = os.path.join(terraformPath, mod)
+    environments = os.listdir(servicePath)
+    pullRequestPaths = []
+
+    for environment in environments:
+        environmentPath = os.path.join(servicePath, environment)
+
+        modulesCachePath = os.path.join(
+            environmentPath, ".terraform/modules/modules.json"
+        )
+
+        with open(modulesCachePath, "r") as f:
+            modulesCacheJson = f.read()
+            f.close()
+
+        modulesCache = json.loads(modulesCacheJson)
+        modulesDependencies = modulesCache.get("Modules")
+
+        for dep in modulesDependencies:
+            moduleSource = os.path.join(environmentPath, dep.get("Source"))
+            prPath = (os.path.relpath(moduleSource) + "/**").replace(path + "/", "")
+
+            if os.path.exists(moduleSource) and prPath not in pullRequestPaths:
+                pullRequestPaths.append(prPath)
+
+    workflow = {
+        "name": "Deploy " + mod,
+        "on": {
+            "pull_request": {"branches": ["main"], "paths": pullRequestPaths},
+            "workflow_dispatch": {},
+        },
+        "jobs": {},
+    }
+
+    workflowYaml = yaml.safe_dump(workflow, sort_keys=False)
+
+    print(workflowYaml)
