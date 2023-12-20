ba600da9435062b2b5736c3b8678ec36a63d2b30
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-20 11:19:28 +0100

    wip

--- scripts/terraform-ci/test.py
+++ scripts/terraform-ci/test.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python3
 
+
 import argparse
 import os
 import glob
-
-
-def getTerraformDirs(path):
-    terraformPath = os.path.join(path, "terraform")
-    dirs = []
-
-    if os.path.isfile(terraformPath):
-        return dirs
-
-    files = glob.glob(terraformPath + "/**/*.tf", recursive=True)
-    modulesPath = os.path.join(terraformPath, "modules")
-    for file in files:
-        # Ignore module files
-        if not file.startswith(modulesPath):
-            dir = os.path.dirname(file)
-            if dir not in dirs:
-                dirs.append(dir)
-
-    return dirs
 
 
 parser = argparse.ArgumentParser()
@@ -29,5 +11,16 @@ parser.add_argument("-p", "--path", type=str, default=".")
 args = parser.parse_args()
 path = args.path
 
-terraformDirs = getTerraformDirs(path)
-print(terraformDirs)
+terraformPath = os.path.join(path, "terraform")
+dirs = []
+
+files = glob.glob(terraformPath + "/**/*.tf", recursive=True)
+modulesPath = os.path.join(terraformPath, "modules")
+for file in files:
+    # Ignore module files
+    if not file.startswith(modulesPath):
+        dir = os.path.dirname(file)
+        if dir not in dirs:
+            dirs.append(dir)
+
+print(dirs)
