5be5465d848621c1043bdcaa842ac8232cb9f37c
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-10-09 08:50:03 +0200

    refactor

--- scripts/oidc/github_azure_oidc.py
+++ scripts/oidc/github_azure_oidc.py
@@ -7,8 +7,8 @@ import subprocess
 
 from azure.identity.aio import DefaultAzureCredential
 from msgraph import GraphServiceClient
-from github import Github
-from github import Auth
+from github import Github as GithubClient
+from github import Auth as GithubAuth
 
 
 def get_github_auth():
@@ -19,10 +19,9 @@ def get_github_auth():
     token = (
         subprocess.run("gh auth token", check=True, stdout=subprocess.PIPE)
         .stdout.decode()
-        .strip()  # "gh auth token" includes a trailing newling. Strip it.
+        .strip()  # "gh auth token" includes a trailing newling. Strip it!
     )
-
-    auth = Auth.Token(token)
+    auth = GithubAuth.Token(token)
     return auth
 
 
@@ -30,7 +29,7 @@ async def get_azure_user():
     """
     Get display name of authenticated Microsoft Azure user.
     """
-    me = await client.me.get()
+    me = await azure_client.me.get()
     if me:
         print("Microsoft Azure user display name: " + me.display_name)
 
@@ -39,18 +38,18 @@ def get_github_user():
     """
     Get name of authenticated GitHub user.
     """
-    g_user_name = g.get_user().name
+    g_user_name = github_client.get_user().name
     print("GitHub user name: " + g_user_name)
 
 
 # Authenticate to Microsoft Azure and print display name of authenticated user.
-credentials = DefaultAzureCredential()
-scopes = ["https://graph.microsoft.com/.default"]
-client = GraphServiceClient(credentials=credentials, scopes=scopes)
+azure_credentials = DefaultAzureCredential()
+azure_scopes = ["https://graph.microsoft.com/.default"]
+azure_client = GraphServiceClient(credentials=azure_credentials, scopes=azure_scopes)
 asyncio.run(get_azure_user())
 
 # Authenticate to GitHub and print name of authenticated user.
-g_auth = get_github_auth()
-g = Github(auth=g_auth)
+github_auth = get_github_auth()
+github_client = GithubClient(auth=github_auth)
 get_github_user()
-g.close()
+github_client.close()
