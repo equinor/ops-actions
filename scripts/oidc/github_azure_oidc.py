b58b4acb49135c37f2c85dd2ba87471a18bae78c
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-10-09 14:44:48 +0200

    List applications and repos

--- scripts/oidc/github_azure_oidc.py
+++ scripts/oidc/github_azure_oidc.py
@@ -2,15 +2,46 @@
 TODO(@hknutsen): write a module description.
 """
 
-import asyncio
 import subprocess
 import requests
 
-from azure.identity.aio import DefaultAzureCredential
-from msgraph import GraphServiceClient
-
-
+MICROSOFT_GRAPH_URL = "https://graph.microsoft.com/v1.0/"
 GITHUB_API_URL = "https://api.github.com"
+
+
+def get_msgraph_token():
+    """
+    Get Microsoft Graph token from Azure CLI.
+    """
+    token = (
+        subprocess.run(
+            "az account get-access-token --resource-type ms-graph --query accessToken --output tsv",
+            shell=True,
+            check=True,
+            stdout=subprocess.PIPE,
+        )
+        .stdout.decode()
+        .strip()  # "az account get-access-token" includes a trailing newling. Strip it!
+    )
+    return token
+
+
+def list_entra_applications(token):
+    """
+    List Entra applications by given display name.
+    """
+    url = f"{MICROSOFT_GRAPH_URL}/applications"
+    headers = {
+        "Authorization": f"Bearer {token}",
+    }
+    params = {}
+    applications = requests.get(
+        url=url,
+        headers=headers,
+        params=params,
+        timeout=300,
+    )
+    return applications.json()
 
 
 def get_github_token():
@@ -25,11 +56,11 @@ def get_github_token():
     return token
 
 
-def get_github_repo(owner, repo, token):
+def list_github_repos(org, token):
     """
     Get a given GitHub repository.
     """
-    url = f"{GITHUB_API_URL}/repos/{owner}/{repo}"
+    url = f"{GITHUB_API_URL}/orgs/{org}/repos"
     headers = {
         "Accept": "application/vnd.github+json",
         "Authorization": f"Bearer {token}",
@@ -45,22 +76,12 @@ def get_github_repo(owner, repo, token):
     return repos.json()
 
 
-async def get_azure_user():
-    """
-    Get display name of authenticated Microsoft Azure user.
-    """
-    me = await azure_client.me.get()
-    if me:
-        print("Microsoft Azure user display name: " + me.display_name)
+# List Microsoft Entra applications.
+msgraph_token = get_msgraph_token()
+entra_applications = list_entra_applications(msgraph_token)
+print(entra_applications)
 
-
-# Authenticate to Microsoft Azure and print display name of authenticated user.
-azure_credentials = DefaultAzureCredential()
-azure_scopes = ["https://graph.microsoft.com/.default"]
-azure_client = GraphServiceClient(credentials=azure_credentials, scopes=azure_scopes)
-asyncio.run(get_azure_user())
-
-# Get a GitHub repository.
+# List GitHub repositories.
 github_token = get_github_token()
-github_repo = get_github_repo("equinor", "ops-actions", github_token)
-print(github_repo["name"])
+github_repos = list_github_repos("equinor", github_token)
+print(github_repos)
