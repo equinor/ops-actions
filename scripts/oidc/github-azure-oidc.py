ef84500671e58ac1a7b3f838a8b35ea59b401572
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-10-09 07:56:14 +0200

    Get current Entra ID user display name

--- scripts/oidc/github-azure-oidc.py
+++ scripts/oidc/github-azure-oidc.py
@@ -3,16 +3,14 @@ import asyncio
 from azure.identity.aio import DefaultAzureCredential
 from msgraph import GraphServiceClient
 
-user_id = ""  # TODO: set user principal name here
 
-
-async def get_user():
-    user = await client.users.by_user_id(user_id).get()
-    if user:
-        print(user.display_name)
+async def me():
+    me = await client.me.get()
+    if me:
+        print(me.display_name)
 
 
 credentials = DefaultAzureCredential()
 scopes = ["https://graph.microsoft.com/.default"]
 client = GraphServiceClient(credentials=credentials, scopes=scopes)
-asyncio.run(get_user())
+asyncio.run(me())
