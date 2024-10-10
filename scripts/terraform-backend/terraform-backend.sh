98a408f53e8c8287f2f0a6720259cbb43c247968
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-10-10 11:20:25 +0200

    chore: only wrap variable refs in brackets if required

--- scripts/terraform-backend/terraform-backend.sh
+++ scripts/terraform-backend/terraform-backend.sh
@@ -36,8 +36,8 @@ SUBSCRIPTION_NAME=$(az account show --query name --output tsv)
 readonly SUBSCRIPTION_NAME
 
 while true; do
-  read -r -p "Create Terraform backend in Azure subscription '${SUBSCRIPTION_NAME}'? (y/N) " response
-  case "${response}" in
+  read -r -p "Create Terraform backend in Azure subscription '$SUBSCRIPTION_NAME'? (y/N) " response
+  case "$response" in
   [yY][eE][sS] | [yY])
     echo "Proceeding with creation..."
     break
@@ -56,26 +56,26 @@ done
 # Read Terraform backend configuration
 ################################################################################
 
-if [[ -f "${CONFIG_FILE}" ]]; then
-  echo "Using config file '${CONFIG_FILE}'."
+if [[ -f "$CONFIG_FILE" ]]; then
+  echo "Using config file '$CONFIG_FILE'."
 else
-  echo "Config file '${CONFIG_FILE}' does not exist."
+  echo "Config file '$CONFIG_FILE' does not exist."
   exit 1
 fi
 
-CONFIG=$(cat "${CONFIG_FILE}")
+CONFIG=$(cat "$CONFIG_FILE")
 readonly CONFIG
 
-RESOURCE_GROUP_NAME=$(echo "${CONFIG}" | jq -r .resource_group_name)
+RESOURCE_GROUP_NAME=$(echo "$CONFIG" | jq -r .resource_group_name)
 readonly RESOURCE_GROUP_NAME
 
-STORAGE_ACCOUNT_NAME=$(echo "${CONFIG}" | jq -r .storage_account_name)
+STORAGE_ACCOUNT_NAME=$(echo "$CONFIG" | jq -r .storage_account_name)
 readonly STORAGE_ACCOUNT_NAME
 
-CONTAINER_NAME=$(echo "${CONFIG}" | jq -r .container_name)
+CONTAINER_NAME=$(echo "$CONFIG" | jq -r .container_name)
 readonly CONTAINER_NAME
 
-USE_AZUREAD_AUTH=$(echo "${CONFIG}" | jq -r .use_azuread_auth)
+USE_AZUREAD_AUTH=$(echo "$CONFIG" | jq -r .use_azuread_auth)
 readonly USE_AZUREAD_AUTH
 
 ################################################################################
@@ -83,8 +83,8 @@ readonly USE_AZUREAD_AUTH
 ################################################################################
 
 STORAGE_ACCOUNT_ID=$(az storage account list \
-  --resource-group "${RESOURCE_GROUP_NAME}" \
-  --query "[?name == '${STORAGE_ACCOUNT_NAME}'].id" \
+  --resource-group "$RESOURCE_GROUP_NAME" \
+  --query "[?name == '$STORAGE_ACCOUNT_NAME'].id" \
   --output tsv)
 
 LOCK_NAME="Terraform"
@@ -93,16 +93,16 @@ readonly LOCK_NAME
 LOCK_ID=""
 if [[ -n "$STORAGE_ACCOUNT_ID" ]]; then
   LOCK_ID=$(az resource lock list \
-    --resource "${STORAGE_ACCOUNT_ID}" \
-    --query "[?name == '${LOCK_NAME}'].id" \
+    --resource "$STORAGE_ACCOUNT_ID" \
+    --query "[?name == '$LOCK_NAME'].id" \
     --output tsv)
 fi
 readonly LOCK_ID
 
-if [[ -n "${LOCK_ID}" ]]; then
+if [[ -n "$LOCK_ID" ]]; then
   echo -e "\n\033[0;33mStorage account is locked."
   echo -e "Please remove the lock by running the following command:"
-  echo -e "\n\033[0;36maz resource lock delete --ids ${LOCK_ID}\033[0m\n"
+  echo -e "\n\033[0;36maz resource lock delete --ids $LOCK_ID\033[0m\n"
   exit 1
 fi
 
@@ -113,8 +113,8 @@ fi
 echo "Creating resource group..."
 
 az group create \
-  --name "${RESOURCE_GROUP_NAME}" \
-  --location "${LOCATION}" \
+  --name "$RESOURCE_GROUP_NAME" \
+  --location "$LOCATION" \
   --output none
 
 ################################################################################
@@ -125,7 +125,7 @@ echo "Creating storage account..."
 
 ALLOW_SHARED_KEY_ACCESS="false"
 ROLE="Storage Blob Data Owner"
-if [[ "${USE_AZUREAD_AUTH}" != "true" ]]; then
+if [[ "$USE_AZUREAD_AUTH" != "true" ]]; then
   ALLOW_SHARED_KEY_ACCESS="true"
   ROLE="Reader and Data Access"
 fi
@@ -133,38 +133,38 @@ readonly ALLOW_SHARED_KEY_ACCESS
 readonly ROLE
 
 DEFAULT_ACTION="Deny"
-if [[ -z "${IP_ADDRESSES}" ]]; then
+if [[ -z "$IP_ADDRESSES" ]]; then
   DEFAULT_ACTION="Allow"
 fi
 readonly DEFAULT_ACTION
 
 STORAGE_ACCOUNT_ID="$(az storage account create \
-  --name "${STORAGE_ACCOUNT_NAME}" \
-  --resource-group "${RESOURCE_GROUP_NAME}" \
-  --location "${LOCATION}" \
+  --name "$STORAGE_ACCOUNT_NAME" \
+  --resource-group "$RESOURCE_GROUP_NAME" \
+  --location "$LOCATION" \
   --sku Standard_GRS \
   --access-tier Hot \
   --kind StorageV2 \
   --https-only true \
   --min-tls-version TLS1_2 \
   --allow-blob-public-access false \
-  --allow-shared-key-access "${ALLOW_SHARED_KEY_ACCESS}" \
+  --allow-shared-key-access "$ALLOW_SHARED_KEY_ACCESS" \
   --allow-cross-tenant-replication false \
-  --default-action "${DEFAULT_ACTION}" \
+  --default-action "$DEFAULT_ACTION" \
   --query id \
   --output tsv)"
 
 for ip_address in $IP_ADDRESSES; do
   az storage account network-rule add \
-    --account-name "${STORAGE_ACCOUNT_NAME}" \
-    --resource-group "${RESOURCE_GROUP_NAME}" \
-    --ip-address "${ip_address}" \
+    --account-name "$STORAGE_ACCOUNT_NAME" \
+    --resource-group "$RESOURCE_GROUP_NAME" \
+    --ip-address "$ip_address" \
     --output none
 done
 
 az storage account blob-service-properties update \
-  --account-name "${STORAGE_ACCOUNT_NAME}" \
-  --resource-group "${RESOURCE_GROUP_NAME}" \
+  --account-name "$STORAGE_ACCOUNT_NAME" \
+  --resource-group "$RESOURCE_GROUP_NAME" \
   --enable-delete-retention true \
   --delete-retention-days 30 \
   --enable-container-delete-retention true \
@@ -174,8 +174,8 @@ az storage account blob-service-properties update \
   --output none
 
 az security atp storage update \
-  --storage-account "${STORAGE_ACCOUNT_NAME}" \
-  --resource-group "${RESOURCE_GROUP_NAME}" \
+  --storage-account "$STORAGE_ACCOUNT_NAME" \
+  --resource-group "$RESOURCE_GROUP_NAME" \
   --is-enabled true \
   --output none
 
@@ -186,8 +186,8 @@ az security atp storage update \
 echo "Creating storage container..."
 
 az storage container create \
-  --name "${CONTAINER_NAME}" \
-  --account-name "${STORAGE_ACCOUNT_NAME}" \
+  --name "$CONTAINER_NAME" \
+  --account-name "$STORAGE_ACCOUNT_NAME" \
   --auth-mode login \
   --output none
 
@@ -197,7 +197,7 @@ az storage container create \
 
 echo "Creating lifecycle policy..."
 
-MANAGEMENT_POLICY=$(echo "${CONFIG}" | jq '{
+MANAGEMENT_POLICY=$(echo "$CONFIG" | jq '{
   rules: [
     {
       name: "Delete old tfstate versions",
@@ -226,9 +226,9 @@ MANAGEMENT_POLICY=$(echo "${CONFIG}" | jq '{
 readonly MANAGEMENT_POLICY
 
 az storage account management-policy create \
-  --account-name "${STORAGE_ACCOUNT_NAME}" \
-  --resource-group "${RESOURCE_GROUP_NAME}" \
-  --policy "${MANAGEMENT_POLICY}" \
+  --account-name "$STORAGE_ACCOUNT_NAME" \
+  --resource-group "$RESOURCE_GROUP_NAME" \
+  --policy "$MANAGEMENT_POLICY" \
   --output none
 
 ################################################################################
@@ -238,9 +238,9 @@ az storage account management-policy create \
 echo "Creating role assignment..."
 
 az role assignment create \
-  --assignee "${OBJECT_ID}" \
-  --role "${ROLE}" \
-  --scope "${STORAGE_ACCOUNT_ID}" \
+  --assignee "$OBJECT_ID" \
+  --role "$ROLE" \
+  --scope "$STORAGE_ACCOUNT_ID" \
   --output none
 
 ################################################################################
@@ -250,8 +250,8 @@ az role assignment create \
 echo "Creating resource lock..."
 
 az resource lock create \
-  --name "${LOCK_NAME}" \
+  --name "$LOCK_NAME" \
   --lock-type ReadOnly \
-  --resource "${STORAGE_ACCOUNT_ID}" \
+  --resource "$STORAGE_ACCOUNT_ID" \
   --notes "Prevent changes to Terraform backend configuration" \
   --output none
